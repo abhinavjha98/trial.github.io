7b6a2ccc0e9e1486c3ef70ab4bbb293408856f1c
Abhinav <abhinavjha98ald@gmail.com>
2018-08-17 11:57:06 +0530

    Made the facebook using for loop

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var database = [
 {
 	username: "Abhi",
 	password: "1234"
+},
+{
+	username: "Dhruv",
+	password: "007"
+},
+{
+	username: "Abhinav",
+	password: "123456"
 }
 ];
 var newsFeed = [{
@@ -12,11 +20,19 @@ var newsFeed = [{
 	timeLine:"Helloe"
 }
 ];
+function userValid(username,password){
+	for (var i = 0; i < database.length; i++) {
+		if(database[i].username===username && database[i].password===password){
+			return true;
+		}
+	}return false;
+}
+
 var userNamePrompt =prompt("What is your username");
 var passwordPrompt= prompt("What is your password");
 
 function signIn(user,pass){
-	if(user===database[0].username && pass===database[0].password){
+	if(userValid(user,pass)){
 		console.log("Hello"+user);
 	}
 	else{
